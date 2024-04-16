272d8b0b3db296347067296423696140d80d52c7
Dawid Wojtaszek <dawidw22@gmail.com>
2024-04-16 13:30:37 +0200

    create footer

---
+++ app/components/footer/footer.jsx
@@ -0,0 +1,7 @@
+const Footer = () => (
+  <footer className="w-full h-[120px] bg-darkBlue text-white flex justify-center items-center">
+    <span>© {new Date().getFullYear()} Dawid Wojtaszek</span>
+  </footer>
+);
+
+export default Footer;
--- app/page.js
+++ app/page.js
@@ -2,13 +2,16 @@ import Header from "./components/header/header";
 import HeroSection from "./components/hero-section/hero-section";
 import Portfolio from "./components/portfolio/portfolio";
 import Contact from "./components/contact/contact";
+import Footer from "./components/footer/footer";
 export default function Home() {
   return (
     <main>
+      {/* TODO Move header and footer to layout */}
       <Header />
       <HeroSection />
       <Portfolio />
       <Contact />
+      <Footer />
     </main>
   );
 }
