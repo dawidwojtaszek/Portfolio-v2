a47568d51971b32621291c0c90a82223712b7962
Dawid Wojtaszek <dawidw22@gmail.com>
2024-04-16 10:45:31 +0200

    add contact animation

--- app/components/contact/contact.jsx
+++ app/components/contact/contact.jsx
@@ -1,12 +1,14 @@
 import Container from "../utilities/container";
 import SectionHeading from "../utilities/sectionHeading";
 import ContactInfo from "./contact-info";
+import ContactSvg from "./contact-svg";
 const Contact = () => (
   <div className=" bg-gray" role="none presentation">
     <Container className="py-[70px]">
       <SectionHeading>Kontakt</SectionHeading>
-      <div className="flex justify-between">
+      <div className="flex justify-between mt-5 items-center">
         <ContactInfo />
+        <ContactSvg />
       </div>
     </Container>
   </div>
