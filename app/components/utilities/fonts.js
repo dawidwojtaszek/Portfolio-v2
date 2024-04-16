64b0a78f75ab69bea2881b54facaee544cb8bfa2
Dawid Wojtaszek <dawidw22@gmail.com>
2024-04-16 12:26:13 +0200

    create fonts utility

---
+++ app/components/utilities/fonts.js
@@ -0,0 +1,11 @@
+import { IBM_Plex_Sans, Space_Grotesk } from "next/font/google";
+
+export const IBM_Plex = IBM_Plex_Sans({
+  subsets: ["latin"],
+  weight: ["400", "600"],
+});
+
+export const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["400", "600"],
+});
