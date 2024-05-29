e59b30476316508729c70fdeab0cf109b274ca56
Dawid Wojtaszek <dawidw22@gmail.com>
2024-05-29 10:57:54 +0200

    fix: font issue

--- app/components/hero-section/hero-info.jsx
+++ app/components/hero-section/hero-info.jsx
@@ -1,11 +1,7 @@
 import { spaceGrotesk } from "../utilities/fonts";
 const HeroInfo = () => (
   <div className=" ">
-    <h1
-      className={`${spaceGrotesk.className} text-darkGray text-[40px] font-bold`}
-    >
-      DAWID WOJTASZEK
-    </h1>
+    <h1 className={` text-darkGray text-[40px] font-bold`}>DAWID WOJTASZEK</h1>
     <span className=" text-2xl text-darkGray font-bold">
       FRONT-END DEVELOPER
     </span>
