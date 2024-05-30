71cefa76ffdc305d7d26a0d02d2eacb2715fe0f0
Dawid Wojtaszek <dawidw22@gmail.com>
2024-05-30 11:27:08 +0200

    fix: wrong place for classname

--- app/components/utilities/sectionHeading.jsx
+++ app/components/utilities/sectionHeading.jsx
@@ -1,9 +1,9 @@
 import { spaceGrotesk } from "./fonts";
 const SectionHeading = ({ children, className }) => (
-  <div className="flex justify-start h-[30px] items-center">
+  <div className={`${className} flex justify-start h-[30px] items-center`}>
     <div className="w-[20px] h-[30px] bg-green mr-2"></div>
     <h2
-      className={`${spaceGrotesk.className} ${className} text-3xl font-bold text-darkGray`}
+      className={`${spaceGrotesk.className} text-3xl font-bold text-darkGray`}
     >
       {children}
     </h2>
