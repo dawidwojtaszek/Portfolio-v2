53648777a10df4cd99850afee29175ebb77f03f4
Dawid Wojtaszek <dawidw22@gmail.com>
2024-05-31 12:10:45 +0200

    fix small styling issues

--- app/projects/[slug]/page.jsx
+++ app/projects/[slug]/page.jsx
@@ -7,54 +7,58 @@ import { faEye } from "@fortawesome/free-solid-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import Image from "next/image";
 import SectionHeading from "@/app/components/utilities/sectionHeading";
+import { notFound } from "next/navigation";
 export default function ProjectsPage({ params: { slug } }) {
   const projectInfo = getSortedProjectsData().find((e) => e.slug === slug);
-
-  return (
-    <div>
-      <div className=" bg-gray md:min-h-[450px]">
-        <Container className="flex flex-col-reverse md:flex-row md:justify-between md:items-center h-full">
-          <div
-            className="flex flex-col h-full justify-center items-center pb-10 md:items-start"
-            role="none presentation"
-          >
-            <h2
-              className={`${spaceGrotesk.className} text-4xl font-bold uppercase max-w-[440px] mb-10`}
+  if (projectInfo != undefined) {
+    return (
+      <div className="min-h-[calc(100vh-200px)]">
+        <div className=" bg-gray md:min-h-[480px]">
+          <Container className="flex flex-col-reverse md:flex-row md:justify-between md:items-center h-full">
+            <div
+              className="flex flex-col h-full justify-center items-center pb-10 md:items-start"
+              role="none presentation"
             >
-              {projectInfo.title}
-            </h2>
-            <div className="flex  gap-4" role="none presentation">
-              <Link href={projectInfo.previewUrl}>
-                <Button className=" w-[170px] h-[50px]" icon={faEye}>
-                  Podgląd
-                </Button>
-              </Link>
-              <Link href={projectInfo.githubUrl}>
-                <Button className=" w-[170px] h-[50px]" icon={faGithub}>
-                  Github
-                </Button>
-              </Link>
+              <h2
+                className={`${spaceGrotesk.className} text-4xl font-bold uppercase max-w-[440px] mb-10`}
+              >
+                {projectInfo.title}
+              </h2>
+              <div className="flex  gap-4" role="none presentation">
+                <Link href={projectInfo.previewUrl}>
+                  <Button className=" w-[170px] h-[50px]" icon={faEye}>
+                    Podgląd
+                  </Button>
+                </Link>
+                <Link href={projectInfo.githubUrl}>
+                  <Button className=" w-[170px] h-[50px]" icon={faGithub}>
+                    Github
+                  </Button>
+                </Link>
+              </div>
             </div>
-          </div>
-          <div className="h-full w-auto " role="none presentation">
-            <Image
-              src={projectInfo.sesoundaryPhotoUrl}
-              alt={`${projectInfo.slug} screenshot`}
-              width={755}
-              height={420}
-            />
-          </div>
+            <div className="h-full w-auto " role="none presentation">
+              <Image
+                src={projectInfo.sesoundaryPhotoUrl}
+                alt={`${projectInfo.slug} screenshot`}
+                width={755}
+                height={420}
+              />
+            </div>
+          </Container>
+        </div>
+        <Container>
+          <SectionHeading className="mb-9 mt-11">O projekcie</SectionHeading>
+          {/* TODO add technology tags */}
+          <div
+            className=" text-xl py-9"
+            dangerouslySetInnerHTML={{ __html: projectInfo.content }}
+            role="none presentation"
+          ></div>
         </Container>
       </div>
-      <Container>
-        <SectionHeading className="mb-9 mt-11">O projekcie</SectionHeading>
-        {/* TODO add technology tags */}
-        <div
-          className=" text-xl py-9"
-          dangerouslySetInnerHTML={{ __html: projectInfo.content }}
-          role="none presentation"
-        ></div>
-      </Container>
-    </div>
-  );
+    );
+  } else {
+    return notFound();
+  }
 }
