db0a6f832902db9285dfefac1dfa0dc9d745775e
Dawid <dawidw22@gmail.com>
2024-09-12 12:03:08 +0200

    add highlighting to active tags

--- app/components/portfolio/tags.jsx
+++ app/components/portfolio/tags.jsx
@@ -1,8 +1,18 @@
+"use client";
+import { useAppContext } from "@/app/context/context";
 const Tags = ({ tags }) => {
+  const { currentFilters } = useAppContext();
   return (
     <div className="flex gap-1 my-2 flex-wrap">
       {tags.map((t, index) => (
-        <div className="bg-green py-1 px-2 rounded-md text-white" key={index}>
+        <div
+          className={`${
+            currentFilters.includes(t)
+              ? "border  border-green font-bold text-green "
+              : "bg-green"
+          } py-1 px-2 rounded-md text-white`}
+          key={index}
+        >
           {t}
         </div>
       ))}
