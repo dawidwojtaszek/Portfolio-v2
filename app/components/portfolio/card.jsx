5618d59a3570d95a94fa29b503b61e377ab62181
Dawid <dawidw22@gmail.com>
2024-09-12 11:13:44 +0200

    update animation

--- app/components/portfolio/card.jsx
+++ app/components/portfolio/card.jsx
@@ -23,9 +23,9 @@ const Card = ({
     <motion.div
       className="border-4 border-darkBlue shadow-shadowEffect flex min-h-[345px] mb-12"
       role="none presentation"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
+      initial={{ opacity: 0, y: 10 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: -10 }}
       transition={{ duration: 0.3 }}
       layout
     >
